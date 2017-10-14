0efa1eec5e90f1e6c9f00147d9becd7cc50d030e
mapcic <mapcic@mail.ru>
2017-10-15 00:31:37 +0300

    Main structure

---
+++ sbb.py
@@ -0,0 +1,30 @@
+class SBBInterface(object):
+	"""docstring for SBBInterface"""
+	def __init__(self, arg):
+		super(SBBInterface, self).__init__()
+		self.arg = arg
+
+class SBBFactory(object):
+	"""docstring for SBBFactory"""
+	def __init__(self, arg):
+		super(SBBFactory, self).__init__()
+		self.arg = arg
+
+class SBB(object):
+	"""docstring for SBB"""
+	def __init__(self, arg):
+		super(SBB, self).__init__()
+		self.arg = arg
+		
+class SBBLearning(object):
+	"""docstring for SBBLearning"""
+	def __init__(self, arg):
+		super(SBBLearning, self).__init__()
+		self.arg = arg
+		
+class SBBCommunication(object):
+	"""docstring for SBBCommunication"""
+	def __init__(self, arg):
+		super(SBBCommunication, self).__init__()
+		self.arg = arg
+		
